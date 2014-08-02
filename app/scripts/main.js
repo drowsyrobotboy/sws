5a71b57b0a8f4854a88742e2950b649855fdd62b
Maruthi Sharma <maruthisharma@live.in>
2014-08-02 23:28:41 +0530

    fixed the button overlap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
       padding:0
     });
     $('.borders').fadeIn("slow");
-    $('.head').css({"top":"100px","opacity":"1"});
+    $('.head').css({"top":"80px","opacity":"1"});
     setTimeout(function(){
       $('.menu').fadeIn("slow");
     },1200);
