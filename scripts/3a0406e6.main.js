8befce74e5ae72fc518d0974ffa074f446f7a2a1
Maruthi Sharma <maruthisharma@live.in>
2014-08-02 20:17:12 +0530

    initial version

---
+++ scripts/3a0406e6.main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".fancybox").fancybox({padding:0}),$(".borders").fadeIn("slow"),$(".head").css({top:"100px",opacity:"1"}),setTimeout(function(){$(".menu").fadeIn("slow")},1200)});
