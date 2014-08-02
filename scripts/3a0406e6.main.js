912ea4afa043a9edb11dd6ad17a5365252e22719
Maruthi Sharma <maruthisharma@live.in>
2014-08-02 23:35:37 +0530

    fixed mobile button overlap

--- scripts/3a0406e6.main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".fancybox").fancybox({padding:0}),$(".borders").fadeIn("slow"),$(".head").css({top:"100px",opacity:"1"}),setTimeout(function(){$(".menu").fadeIn("slow")},1200)});
